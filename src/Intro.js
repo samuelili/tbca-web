3c0bb0a1ed3c225e1bfdb719346d7057eb91e9f1
mashedpotats <samuelli4521@gmail.com>
2017-01-30 20:40:12 -0800

    added Intro component

--- src/Intro.js
+++ src/Intro.js
@@ -1,19 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './bower_components/bootstrap/dist/css/bootstrap.min.css';
-// import './bower_components/jquery/dist/jquery.slim.min.js'
 import './App.css';
 
-class Intro extends Component {
-  render() {
-    return (
-      <div className="intro">
-        <div className="content">
-          <h3>We are</h3>
-          <h1 id="animate-tbca-title"/>
-          <p>Utilizing blockchain technologies to change the world.</p></div>
-      </div>
-    );
-  }
+class Base extends Component {
+    render() {
+        return (
+            <div className="intro">
+                <div className="content">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Intro;
+class AnimateText extends Component {
+    constructor(props) {
+        super();
+        this.titleChars = props.title.split('');
+        this.titleChars.unshift('');
+        this.interval = props.interval;
+        this.state = {text: ""};
+    }
+
+    componentDidMount() {
+        let index = 0;
+        let ref = this;
+
+        this.timerId = setInterval(function() {
+            if (index !== ref.titleChars.length) {
+                ref.setState({
+                    text: ref.state.text + ref.titleChars[index]
+                });
+                console.log(ref.titleChars);
+                index++;
+            } else
+                clearInterval(ref.timerId);
+        }, this.interval);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    render() {
+        return (
+            <h1 className="animate-intro-title">{this.state.text}</h1>
+        )
+    }
+}
+
+export default {
+    Base: Base,
+    AnimateText: AnimateText
+};
--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import './index.css';
 
 ReactDOM.render(
   <div id="root">
-    <Intro/>
+      <Intro.Base>
+          <h3>We are</h3>
+          <Intro.AnimateText title="tbca" interval="376"/>
+          <p>Utilizing blockchain technologies to change the world.</p>
+      </Intro.Base>
     <App />
   </div>,
   document.querySelector('#app')
