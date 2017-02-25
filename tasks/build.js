5e069afd3b9c06abedf15d39d66c79ac4ecfe1f9
Samuel Li <Samuel Li>
2017-02-24 18:23:59 -0800

    added news page

--- tasks/build.js
+++ tasks/build.js
@@ -5,15 +5,33 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const marked = require('marked');
 
 const path = require('path');
 const paths = require('./paths');
+const fs = require('fs');
 
 gulp.task('build', ['build:pug', 'build:sass']);
 
 gulp.task('build:pug', function () {
+    var articlesRaw = JSON.parse(fs.readFileSync('articles/news.json', 'utf-8'));
+    var articles = [];
+    for (var i = 0; i < articlesRaw.length; i++) {
+        var article = articlesRaw[i];
+        articles.push({ // copy data and read from mdFile
+            title: article.title,
+            date: article.date,
+            markdown: marked(fs.readFileSync('articles/' + article.mdFile, 'utf-8')),
+            fullLink: article.fullLink
+        });
+    }
+
     return gulp.src(path.join(paths.src, '**/[^_]*.pug'))
-        .pipe(pug().on('error', function(e) {
+        .pipe(pug({
+            data: {
+                articles: articles
+            }
+        }).on('error', function (e) {
             console.log(e);
         }))
         .pipe(gulp.dest(paths.dest));
